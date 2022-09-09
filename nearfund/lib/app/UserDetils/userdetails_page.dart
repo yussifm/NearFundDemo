0a51469b0c4b31da45feae1ef5ced60448dfd118
yussifm13 <yussifm13@gmail.com>
2022-09-09 23:44:08 +0000

    user

--- nearfund/lib/app/UserDetils/userdetails_page.dart
+++ nearfund/lib/app/UserDetils/userdetails_page.dart
@@ -1,12 +1,19 @@
 // ignore_for_file: non_constant_identifier_names
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:nearfund/Utils/form_validator.dart';
 import 'package:nearfund/app/Auth/widgets/auth_btn.dart';
+import 'package:nearfund/app/mainPage/main_page.dart';
+import 'package:nearfund/models/user_model.dart';
 import 'package:nearfund/theme/colors.dart';
 import 'package:nearfund/widgets/general_textfiled.dart';
+
+import '../../services/UserService.dart';
+import '../../services/authService.dart';
 
 class UserDetailsPage extends ConsumerStatefulWidget {
   UserDetailsPage({Key? key}) : super(key: key);
@@ -20,15 +27,18 @@ enum UserType { student, content }
 class _UserDetailsPageState extends ConsumerState<UserDetailsPage> {
   late TextEditingController _about_controller;
   late TextEditingController _name_controller;
+  late TextEditingController _phone_controller;
   late TextEditingController _link_controller;
+  final _userformKey = GlobalKey<FormState>();
   UserType? userType = UserType.content;
+  bool _loading = false;
 
   @override
   void initState() {
     _about_controller = TextEditingController();
     _link_controller = TextEditingController();
     _name_controller = TextEditingController();
-
+    _phone_controller = TextEditingController();
     super.initState();
   }
 
@@ -37,8 +47,56 @@ class _UserDetailsPageState extends ConsumerState<UserDetailsPage> {
     _about_controller.dispose();
     _name_controller.dispose();
     _link_controller.dispose();
-
+    _phone_controller.dispose();
     super.dispose();
+  }
+
+  bool validationAndSave() {
+    final form = _userformKey.currentState;
+    if (form!.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
+  }
+
+  validatedAndSubmit({required WidgetRef ref, required String userType}) async {
+    final autState = ref.watch(authServiceProvide);
+    final userState = ref.watch(userServiceProvide);
+
+    if (validationAndSave()) {
+      setState(() {
+        _loading = true;
+      });
+      try {
+        await userState
+            .createUser(
+                user: UserModel(
+                    uId: autState.getCurrentUser()!.uid,
+                    name: _name_controller.text,
+                    about: _about_controller.text,
+                    imgUrl: '',
+                    userType: userType,
+                    userLink: _link_controller.text,
+                    contact: _phone_controller.text,
+                    createdAt: FieldValue.serverTimestamp()),
+                userId: autState.getCurrentUser()!.uid)
+            .then((value) async {
+          Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => MainAppPage()));
+        });
+
+        setState(() {
+          _loading = false;
+        });
+      } catch (e) {
+        setState(() {
+          _loading = false;
+
+          return;
+        });
+      }
+    }
   }
 
   @override
@@ -154,6 +212,7 @@ class _UserDetailsPageState extends ConsumerState<UserDetailsPage> {
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20),
             child: Form(
+              key: _userformKey,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
@@ -166,6 +225,20 @@ class _UserDetailsPageState extends ConsumerState<UserDetailsPage> {
                       return null;
                     },
                     controller: _name_controller,
+                    borderRadius: 20,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  filedtextmethod(name: "Contact/Momo number"),
+                  GeneralTextFelidFormWidget(
+                    hint_text: '+2330540000000',
+                    label_text: 'Phone',
+                    validator: (val) {
+                      if (!val!.isValidPhone) return 'Enter valid phone number';
+                      return null;
+                    },
+                    controller: _phone_controller,
                     borderRadius: 20,
                   ),
                   const SizedBox(
